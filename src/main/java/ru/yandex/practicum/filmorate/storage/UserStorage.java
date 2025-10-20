3b6e556ce2eec0b32f9f5a52beac5c3916cd428d
Denis <dessitnov22@yandex.ru>
2025-10-20 21:01:48 +0300

    new

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,10 +6,8 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface UserStorage {
-
     Collection<User> getUsers();
     User addUser(User user);
     User updateUser(User newUser);
     Optional<User> getUserById(Long userId);
-
 }
